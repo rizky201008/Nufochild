9a2ab7e3881a5e104e8e61964e7afce7127458dc
Rizky Agung Prayogi <rizkyden06@gmail.com>
2023-05-29 18:39:08 +0700

    add new screens

--- app/src/main/java/com/nufochild/MainActivity.kt
+++ app/src/main/java/com/nufochild/MainActivity.kt
@@ -6,16 +6,21 @@ import androidx.activity.compose.setContent
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Surface
-import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.tooling.preview.Preview
+import androidx.navigation.NavHostController
+import androidx.navigation.compose.NavHost
+import androidx.navigation.compose.composable
+import androidx.navigation.compose.rememberNavController
+import com.nufochild.screen.SplashScreen
 import com.nufochild.ui.theme.NufochildTheme
 
-sealed class Destination(route: String) {
+sealed class Destination(val route: String) {
     object Home : Destination("home")
     object SplashScreen : Destination("splash")
     object Register : Destination("register")
+    object Login : Destination("login")
+    object Foods : Destination("foods")
 }
 
 class MainActivity : ComponentActivity() {
@@ -28,8 +33,17 @@ class MainActivity : ComponentActivity() {
                     modifier = Modifier.fillMaxSize(),
                     color = MaterialTheme.colorScheme.background
                 ) {
+                    val navController = rememberNavController()
+                    NavigationAppHosts(navController = navController)
                 }
             }
         }
+    }
+}
+
+@Composable
+fun NavigationAppHosts(navController: NavHostController) {
+    NavHost(navController = navController, startDestination = "splash") {
+        composable(Destination.SplashScreen.route) { SplashScreen(navController) }
     }
 }
---
+++ app/src/main/java/com/nufochild/screen/SplashScreen.kt
@@ -0,0 +1,47 @@
+package com.nufochild.screen
+
+import android.content.res.Configuration
+import androidx.compose.foundation.Image
+import androidx.compose.foundation.background
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.padding
+import androidx.compose.material3.MaterialTheme
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.platform.LocalContext
+import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
+import androidx.navigation.NavController
+import com.nufochild.R
+
+@Composable
+fun SplashScreen(navController: NavController) {
+    Column(
+        modifier = Modifier
+            .fillMaxSize()
+            .background(MaterialTheme.colorScheme.primary),
+        horizontalAlignment = Alignment.CenterHorizontally,
+        verticalArrangement = Arrangement.Center
+    ) {
+        Image(
+            modifier = Modifier
+                .padding(10.dp),
+            painter = painterResource(id = R.drawable.logo_transparan),
+            contentDescription = null
+        )
+    }
+}
+
+@Preview(
+    showSystemUi = true, showBackground = true,
+    uiMode = Configuration.UI_MODE_NIGHT_NO or Configuration.UI_MODE_TYPE_NORMAL
+)
+@Composable
+fun SplashScreenPreview() {
+    val ctx = LocalContext.current
+    SplashScreen(navController = NavController(ctx))
+}
